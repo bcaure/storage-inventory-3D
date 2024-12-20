ba667b4939309124531ef7fa4eba34081c81ff24
benjamin.caure_ext@michelin.com <benjamin.caure_ext@michelin.com>
2024-12-20 11:52:44 +0100

    :construction_worker: change output dir and public dir for gitlab pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public',
+    emptyOutDir: true, // also necessary
+  },
+  publicDir: 'static',
 })
